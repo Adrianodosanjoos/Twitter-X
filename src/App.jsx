c49a9c4af37d768afd8c9154841b8c3f8ed12aac
adrianodosanjoos <adriano.luizysa@gmail.com>
2024-10-01 18:35:10 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import { Sidebar } from "./components/Sidebar"
+
+function App() {
+
+  return (
+
+    <div className="flex mx-auto max-w-7xl">
+      <Sidebar />
+    </div>
+
+
+  )
+}
+
+export default App
