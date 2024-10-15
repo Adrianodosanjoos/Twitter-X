76c0e2e7d6a7cf1ffc2f7cdc3d1e88b7bdea6047
adrianodosanjoos <adriano.luizysa@gmail.com>
2024-10-15 18:33:34 -0300

    ....

--- src/utils/generateImagens.js
+++ src/utils/generateImagens.js
@@ -5,14 +5,10 @@ export const getAvatar = (email) => {
     return `https://www.gravatar.com/avatar/${hash}?s=40&d=identicon`;
 }
 
-
-
 export const getRandomImage = () => {
-    const url = `https:picsum.photos/600/400?random=${Math.random()}`
-   
-    return url;
-  };
-
-/*export const getRandomImage = () => {
     return `https:picsum.photos/600/400?random=${Math.random()}`
-}*/
+};
+
+
+ 
+
